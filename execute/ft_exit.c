c553b505ee0f560060326fdc2ab09bd89a2a2268
abel-haj <abel-haj@student.1337.ma>
2021-12-04 22:13:30 +0100

    added options hanlding for exit

--- execute/ft_exit.c
+++ execute/ft_exit.c
@@ -2,10 +2,19 @@
 
 void    ft_exit()
 {
-	// rl_replace_line("", 0);
+	unsigned char	e;
+
 	if (g_global->sig_exdeja == 1)
     	printf("exit");
 	else
 		printf("exit\n");
-    exit(0);
+	if (g_global->lst->options[1] == NULL)
+	    exit(EXIT_SUCCESS);
+	else if (g_global->lst->options[2])
+		printf("minishell: too many arguments\n");
+	else
+	{
+		e = ft_atoi(g_global->lst->options[1]);
+		exit(e);
+	}
 }
